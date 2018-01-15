d0875f290682a20d7f3a75b88e689dc88f8af153
Marc Gilleron <marc.gilleron@gmail.com>
2018-01-16 00:01:10 +0100

    Fixed wrong entry point exports on Windows

--- addons/zylann.heightmap/src/init.cpp
+++ addons/zylann.heightmap/src/init.cpp
@@ -2,17 +2,17 @@
 
 #include "height_map.h"
 
-extern "C" void godot_gdnative_init(godot_gdnative_init_options *o) {
+extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
 
 	godot::Godot::gdnative_init(o);
 }
 
-extern "C" void godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
+extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
 
 	godot::Godot::gdnative_terminate(o);
 }
 
-extern "C" void godot_nativescript_init(void *handle) {
+extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
 
 	godot::Godot::nativescript_init(handle);
 
